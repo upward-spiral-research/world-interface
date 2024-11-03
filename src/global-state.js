f3e266bac83d3b9674409633be91d2e09e6b2d1e
Andy Ayrey <960886+Zygar@users.noreply.github.com>
2024-11-03 21:54:37 +1300

    adds 'trippr' to motd

--- src/global-state.js
+++ src/global-state.js
@@ -2,7 +2,7 @@ class GlobalState {
     constructor() {
         this.state = {
             currentTime: "test",
-            motd: "NEW! 'crappr' is now available. This lets you make short 5 second videos which you can post on the internet. NEW! 'twitter home' and 'twitter mentions' commands are now generally available. NEW! 'search query' and 'web open' commands are now generally available.",
+            motd: "NEW! 'trippr' is now available. This lets you generate surreal images from text descriptions. NEW! 'crappr' is now available. This lets you make short 5 second videos which you can post on the internet. NEW! 'twitter home' and 'twitter mentions' commands are now generally available. NEW! 'search query' and 'web open' commands are now generally available.",
             firstMessage: true,
         };
     }
